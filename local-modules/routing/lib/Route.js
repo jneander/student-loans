380001f7f45488bf7d6c0be6eb0a11881f9b0f86
Jeremy Neander <jeremy.neander@gmail.com>
2017-05-21 10:36:46 -0500

    WIP more stuff

--- local-modules/routing/lib/Route.js
+++ local-modules/routing/lib/Route.js
@@ -1,18 +1,20 @@
 import UrlPattern from 'url-pattern';
 
 class Route {
-  constructor (activityName, path, details = {}) {
+  constructor (name, contexts, path, details = {}) {
     this.internals = {
       urlPattern: new UrlPattern(path)
     };
-    this.activityName = activityName;
+    this.name = name;
+    this.contexts = contexts;
     this.path = path;
     this.details = details;
   }
 
   buildActivity (path, query) {
     return {
-      name: this.activityName,
+      name: this.name,
+      contexts: this.contexts.map(context => context.name),
       ...this.internals.urlPattern.match(path),
       query
     }
@@ -32,9 +34,16 @@ class Route {
     return !!this.internals.urlPattern.match(path);
   }
 
-  buildUrl (pathValues, query = {}) {
-    const pathString = this.internals.urlPattern.stringify(pathValues);
+  buildUrl (...args) {
+    const pathValueMap = {};
+    this.internals.urlPattern.names.forEach((name, index) => {
+      pathValueMap[name] = args[index];
+    });
+    const pathString = this.internals.urlPattern.stringify(pathValueMap);
+
+    const query = args[this.internals.urlPattern.names.length] || {};
     const queryString = Object.keys(query).length > 0 ? `?${qs.stringify(query)}` : '';
+
     return pathString + queryString;
   }
 }
--- local-modules/routing/lib/Router.js
+++ local-modules/routing/lib/Router.js
@@ -1,17 +1,27 @@
 import Route from 'routing/lib/Route';
 
 class Router {
-  constructor () {
+  constructor (contexts = []) {
     this.internals = {
+      contexts,
       routeList: []
     };
     this.urls = {};
   }
 
   add (name, path, details = {}) {
-    const route = new Route(name, path, details);
+    const contextPaths = this.internals.contexts.map(context => context.path);
+    const fullPath = [...contextPaths, path].join(''); // maybe trim slashes to ensure presence
+    const route = new Route(name, this.internals.contexts, fullPath, details);
     this.internals.routeList.push(route);
     this.urls[`${name}Url`] = route.buildUrl.bind(route);
+  }
+
+  context (name, path, defineFn) {
+    const context = new Router([...this.internals.contexts, { name, path }]);
+    defineFn(context);
+    this.internals.routeList = this.internals.routeList.concat(context.internals.routeList);
+    Object.assign(this.urls, context.urls);
   }
 
   match (path, query) {
