9aea9307e20ccdea94d349e0a76d1a8be85e6e84
Jeremy Neander <jneander@instructure.com>
2017-12-10 14:26:03 -0600

    refactoring projection type and too much else

--- local-modules/finance/Projection.js
+++ local-modules/finance/Projection.js
@@ -1,11 +1,7 @@
+import Day from 'units/Day';
+
 import ProjectionV1 from './lib/Projection/ProjectionV1';
 import ProjectionState from './lib/Projection/ProjectionState';
-
-function createDateKey (date) {
-  const month = `${date.getMonth() + 1}`.padStart(2, '0');
-  const day = `${date.getDate()}`.padStart(2, '0');
-  return `${date.getFullYear()}/${month}/${day}`;
-}
 
 export default class Projection {
   constructor (options) {
@@ -18,30 +14,17 @@ export default class Projection {
       this.projection.run();
     }
 
-    this.timelineData = this.projection.timeline().reduce((map, dateActivityMap) => (
-      { ...map, [createDateKey(dateActivityMap.date)]: dateActivityMap }
-    ), {});
+    this._timelineData = {};
+    this.projection._timeline.forEach((dateActivityMap) => {
+      this._timelineData[dateActivityMap.date.toString()] = new ProjectionState(dateActivityMap.date, dateActivityMap);
+    });
   }
 
   getStateAtDate (date) {
-    const events = [];
-    let timelineDatum = this.timelineData[date.toString()];
-
-    if (timelineDatum) {
-      timelineDatum = { ...timelineDatum };
-      delete timelineDatum.date;
-    } else {
-      timelineDatum = {};
+    const dateKey = date.toString();
+    if (!this._timelineData[dateKey]) {
+      this._timelineData[dateKey] = new ProjectionState(date); // empty state
     }
-
-    return { date, events: Object.values(timelineDatum) };
-  }
-
-  timeline2 () {
-    // return this.projection.timeline();
-  }
-
-  timeline () {
-    return this.projection.timeline();
+    return this._timelineData[dateKey];
   }
 }
--- local-modules/finance/lib/AccountState.js
+++ local-modules/finance/lib/AccountState.js
@@ -1,5 +1,30 @@
 export default class AccountState {
-  constructor (account, state) {
+  constructor (account, state = {}) {
+    this.account = account;
+    this.state = {
+      interest: 0,
+      principal: 0,
+      ...state
+    };
+  }
 
+  getPrincipal () {
+    return this.state.principal;
+  }
+
+  getInterest () {
+    return this.state.interest;
+  }
+
+  getBalance () {
+    return this.getPrincipal() + this.getInterest();
+  }
+
+  adjustBalance (amount) {
+  }
+
+  applyInterest (days) {
+    const periodRate = !!days ? (this.apr * days / 365) : this.apr / 12;
+    this.interest += floatToDollars(this.principal * periodRate);
   }
 }
