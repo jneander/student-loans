965ebdff9d59b714cb2224cebcf760c79f9d2bb9
Jeremy Neander <jneander@instructure.com>
2017-12-16 15:00:43 -0600

    add Account and LoanAccount

---
+++ packages/money-rules/local-modules/finance/lib/accounts/Account.js
@@ -0,0 +1,43 @@
+import { Day } from 'units';
+
+export default class Account {
+  constructor (attr) {
+    this._attr = {
+      ...attr,
+      originationDate: new Day(attr.originationDate),
+      updateDate: Day.from(attr.updateDate)
+    };
+  }
+
+  get key () {
+    return this._attr.key;
+  }
+
+  get dailyInterest () {
+    return 0;
+  }
+
+  get balance () {
+    return this._attr.balance;
+  }
+
+  adjustBalance (amount) {
+    this._attr.balance += amount;
+  }
+
+  set updateDate (date) {
+    this._attr.updateDate = Day.from(date);
+  }
+
+  get updateDate () {
+    return this._attr.updateDate;
+  }
+
+  clone () {
+    return new this.constructor(this._attr);
+  }
+
+  toJSON () {
+    return { ...this._attr };
+  }
+}
