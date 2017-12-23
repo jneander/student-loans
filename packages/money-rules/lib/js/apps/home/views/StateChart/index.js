0e00dbcd27c7d36cd8e1b89392fe846b02e156b5
Jeremy Neander <jneander@instructure.com>
2017-12-23 17:22:05 -0500

    replace chart with recharts

--- packages/money-rules/lib/js/apps/home/views/StateChart/index.js
+++ packages/money-rules/lib/js/apps/home/views/StateChart/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { Chart } from 'react-google-charts';
+import { AreaChart, Area, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import Checkbox from '@instructure/ui-core/lib/components/Checkbox';
 import Container from '@instructure/ui-core/lib/components/Container';
 
-import StateChartModel from '../../models/StateChart';
+import Day from 'units/Day';
+
+import Presenter from './Presenter';
+
+function formatAmount (value) {
+  return value.toFixed(2);
+}
+
+function formatDate (date, what) {
+  return new Day(date).toString();
+}
+
+function getDateFromRow (row) {
+  return row.date;
+}
 
 function buildState (props) {
-  const model = new StateChartModel(props.projection, {
+  const model = new Presenter(props.projection, {
     accounts: props.accounts,
     endDate: props.endDate,
     startDate: props.startDate
   });
-  return model.toTable();
-}
+  return {
+    dataSpecs: model.dataSpecs,
+    rows: model.rows
+  }; }
+
+const COLORS = [
+  { fill: '#FF9C9C', stroke: '#FF5757' },
+  { fill: '#B4DDE0', stroke: '#81C3C9' },
+  { fill: '#C9B5E8', stroke: '#958FDB' },
+  { fill: '#B4E0B4', stroke: '#81C981' },
+  { fill: '#B4C6E0', stroke: '#819FC9' },
+  { fill: '#F7F7A3', stroke: '#F5F536' },
+  { fill: '#E8B5CF', stroke: '#DB8FC2' },
+  { fill: '#FFCD9C', stroke: '#FFAB57' }
+];
 
 export default class StateChart extends React.PureComponent {
   constructor (props) {
@@ -36,25 +63,29 @@ export default class StateChart extends React.PureComponent {
 
   render () {
     return (
-      <div>
-        <Container as="div" margin="0 0 medium 0">
-          <Checkbox label="Overall" checked={this.state.showOverall} onChange={this.onShowOverallChange} />
-        </Container>
+      <div style={{ width: '100%', height: '400px' }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart data={this.state.rows} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+            <XAxis dataKey={getDateFromRow} tickCount={10} tickFormatter={formatDate} />
+            <YAxis />
 
-        <Chart
-          chartType={this.state.showOverall ? 'SteppedAreaChart' : 'LineChart'}
-          columns={this.state.columns}
-          rows={this.state.rows}
-          options={{
-            hAxis: { title: 'Date' },
-            isStacked: this.state.showOverall,
-            vAxis: { format: 'currency', title: 'Balance' }
-          }}
-          graph_id="AreaChart"
-          width="100%"
-          height="400px"
-          legend_toggle
-        />
+            <CartesianGrid strokeDasharray="3 3" />
+
+            <Tooltip formatter={formatAmount} />
+
+            {
+              this.state.dataSpecs.map((dataSpec, index) => (
+                <Area
+                  key={dataSpec.key}
+                  dataKey={dataSpec.key}
+                  stackId="1"
+                  type="stepAfter"
+                  {...COLORS[(index + COLORS.length) % COLORS.length]}
+                />
+              ))
+            }
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     );
   }
